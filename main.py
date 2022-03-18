7819726dbe8a971d9cf5b8fda391e997930f23a5
craigv <vear2ev2.co.uk>
2022-03-18 10:53:29 +0000

    adding audio service

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-# get python libraries
 import numpy as np
 import pyaudio
 from glob import glob
-from queue import Queue
 from threading import Thread
 from random import randrange
 import simpleaudio
@@ -43,8 +41,7 @@ class BBBot1:
     def __init__(self, robot=False):
         # own the robot
         self.robot = robot
-        # todo - delete this once beta tested
-        if self.robot:
+        if self.robot:         # todo - delete this once beta tested
             from robot import robot
             self.jetbot = robot.Robot()
 
@@ -53,11 +50,7 @@ class BBBot1:
         self.CHUNK = 2 ** 11
         self.RATE = 44100
         p = pyaudio.PyAudio()
-        self.stream = p.open(format=pyaudio.paInt16,
-                                          channels=2,
-                                          rate=self.RATE,
-                                          input=True,
-                                          frames_per_buffer=self.CHUNK)
+        self.stream = p.open(format=pyaudio.paInt16, channels=2, rate=self.RATE, input=True, frames_per_buffer=self.CHUNK)
 
         # set up audio functions
         self.leftAudioQ = [] #Queue(maxsize=1)
@@ -72,9 +65,7 @@ class BBBot1:
     def main(self):
         while self.running:
             # set up listening stream
-            data = np.frombuffer(self.stream.read(self.CHUNK,
-                                                  exception_on_overflow=False),
-                                 dtype=np.int16)
+            data = np.frombuffer(self.stream.read(self.CHUNK, exception_on_overflow=False), dtype=np.int16)
 
             # grab the RMS for left and right channels
             peakLeft = np.average(np.abs(data[0])) * 2
@@ -95,7 +86,6 @@ class BBBot1:
                 left_speed = round(peakRight / 10000, 1)
                 # add to Right wheel audio Queue
                 self.rightAudioQ.append(left_speed)
-
             else:
                 left_speed = 0
                 right_speed = 0
@@ -103,7 +93,6 @@ class BBBot1:
             # send the command to the robot wheels
             if self.robot:
                 self.jetbot.set_motors(left_speed, right_speed)
-
 
 if __name__ == "__main__":
     bot = BBBot1(robot = False)
